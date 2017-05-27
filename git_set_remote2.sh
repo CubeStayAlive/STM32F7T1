7aad3da960f6def968609ba27f02c6b72b4b25a4
CubeStayAlive <CubeStayAlive@gmx.net>
2017-05-27 18:48:31 +0200

    change repro

--- git_set_remote2.sh
+++ git_set_remote2.sh
@@ -1,6 +1,6 @@
 git remote remove origin
 git config --remove-section branch.master
-git remote add origin https://CubeStayAlive@github.com/CubeStayAlive/STM32F4T1.git
+git remote add origin https://CubeStayAlive@github.com/CubeStayAlive/STM32F7T1.git
 git config branch.master.remote origin
 git config branch.master.merge refs/heads/master 
 #
