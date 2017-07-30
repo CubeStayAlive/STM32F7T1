01ac20a7df97e55cf53e6ac13f9674bc23c403cd
CubeStayAlive <CubeStayAlive@gmx.net>
2017-07-30 12:14:21 +0200

    remote script

--- git_set_remote2.sh
+++ git_set_remote2.sh
@@ -1,12 +1,13 @@
 git remote remove origin
 git config --remove-section branch.master
-git remote add origin https://CubeStayAlive@github.com/CubeStayAlive/STM32F7T1.git
+git remote add origin https://CubeStayAlive:put_password_here@github.com/CubeStayAlive/STM32F7T1.git
 git config branch.master.remote origin
 git config branch.master.merge refs/heads/master 
 #
 git config --remove-section user
 git config user.name CubeStayAlive
 git config user.email CubeStayAlive@gmx.net 
+git config push.default simple
 
 echo ------
 # echo must do manual remove in config
